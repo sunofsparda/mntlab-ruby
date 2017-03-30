3638a1e695dd48fb4632a22047bc1544b5c83a6b
Anton Cherlyonok <sunofsparda@gmail.com>
2017-03-30 17:05:32 +0300

    hw1.1 completed; fixed misses in hw1.2, now completed

--- anton_cherlyonok_l1.rb
+++ anton_cherlyonok_l1.rb
@@ -8,10 +8,20 @@ a. Есть 4 ведра, каждое своего цвета: красн
 чем в жёлтом или в красном и синем вместе?	
 =end
 
+buckets = {red: 50, green: 100, blue: 30, yellow: 60}
 
+# Какой цвет у ведра с максимальны количеством шаров?
+bucket_with_max_balls = buckets.invert[(buckets.values.max)]
+p "Maximum balls in #{bucket_with_max_balls} bucket. (dict inversion)"
 
+bucket_with_max_balls = buckets.key(buckets.values.max)
+p "Maximum balls in #{bucket_with_max_balls} bucket."
 
-
+# Больше ли шаров в зеленом ведре чем в жёлтом или в красном и синем вместе?
+if buckets[:green] > buckets[:yellow] then p "Green bucket has more balls than yellow."
+  elsif buckets[:green] > buckets[:red] + buckets[:blue] then p "There are more balls in green bucket than in red and blue"
+else p "No.."
+end
 
 
 =begin
@@ -23,16 +33,19 @@ b. Курс продажи доллара 11170 + 30% сбор. 
 
 cash = 5600000
 currency = 11370
-ice_cream = 10000
 tax = 0.3
+usd = 270
+ice_cream = 10000
 
-real_currency = currency + currency * tax
+real_currency = (currency + currency * tax).to_i
+byr_need = (usd * real_currency).to_i
 usd = (cash / real_currency).to_i
 change = (cash - usd * real_currency).to_i
 
 p "1 USD = #{real_currency} BYR (with tax)"
+p "I need #{byr_need} BYR to buy #{usd} USD"
 if change >= ice_cream
-	p "I will buy #{usd} USD, and have #{change} BYR left, so i can buy an ice cream ^_^"
+	p "I will buy #{usd} USD and have #{change} BYR left, so i can buy an ice cream ^_^"
 else
-	p "I will buy #{usd} USD, and have #{change} BYR left.. I can't buy an ice cream T_T"
+	p "I will buy #{usd} USD and have #{change} BYR left.. I can't buy an ice cream T_T"
 end
